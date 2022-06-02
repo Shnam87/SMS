7bc407c8cf2d6a763a7f0942a4f38578c48baf9c
SteffanieE <steffanie.eklind@gmail.com>
2022-06-02 14:57:52 +0200

    kopplat style.css och template

---
+++ Classes/Template.php
@@ -0,0 +1,41 @@
+<?php
+
+class Template {
+
+    public static function header($title) 
+    { ?>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title><?= $title ?> - Skön text</title>
+            <link rel="stylesheet" href="./assets/style.css">
+        </head>
+        <body>
+
+            <h1><?=$title?></h1>
+
+            <nav>
+                <a href="/sms">Home</a> |
+                <a href="/sms/pages/products.php">Products</a> |
+                <a href="/sms/pages/contact.php">Contact</a> |
+                <a href="/sms/pages/login.php">Login</a> |
+                <a href="/sms/pages/register.php">Register</a>
+                
+            </nav>
+    <?php  }
+
+    
+    public static function footer() 
+    { ?>
+        <footer>
+            Copyright SMS 2022
+        </footer>
+            
+    </body>
+    </html>
+
+    <?php  }    
+}
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ ."/classes/Template.php";
+
+
+Template::header("SMS");
+?>
+
+    <h2>Om SMS</h2>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Quod ullam veniam harum modi id quam unde omnis culpa. 
+        Ut quas commodi est eaque suscipit, expedita tempore sequi iure molestias recusandae!
+    </p>
+
+<?php    
+
+Template::footer();
+
+?>
