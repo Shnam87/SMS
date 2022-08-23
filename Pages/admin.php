7162011766844564eb3b94e8e8593f64ccd6913c
shnam87 <shahramnamjo2000@gmail.com>
2022-08-23 15:52:40 +0200

    uppdaterat admin sidan

--- Pages/admin.php
+++ Pages/admin.php
@@ -19,7 +19,7 @@ $products = $products_db->get_all();
 
 $order_db = new DatabaseOrders();
 $orders = $order_db->get_all();
-$statuses = $order_db->statuses(); 
+$statuses = $order_db->statuses();
 
 
 
@@ -37,7 +37,7 @@ if (!$isLoggedIn || !$isAdmin) {
     die();
 }
 ?>
-
+<!--
 <nav class="admin-nav">
     <a href="#admin-user-container">USERS</a>
     <a href="#admin-product-container">PRODUCTS</a>
@@ -45,81 +45,89 @@ if (!$isLoggedIn || !$isAdmin) {
 </nav>
 
 <h1>ADMIN</h1>
+ -->
 
-<div id="admin-user-container">
-    <h2>FOR USERS</h2>
-    <?php foreach ($users as $user) : /*var_dump($user)*/ ?>
+<button class="accordion">Manage users:
+    <p class="underrubrik"> Click to edit users </p>
+</button>
+<div class="panel">
+    <div id="admin-user-container">
+        <!-- <h2>FOR USERS</h2> -->
+        <?php foreach ($users as $user) : /*var_dump($user)*/ ?>
 
-        <form action="/sms/scripts/update-user.php" method="post">
-            <input type="text" class="user-info" required name="username" placeholder="Username" value="<?= $user->username ?>">
-            <input type="text" class="user-info" required name="role" placeholder="Role" value="<?= $user->role ?>">
-            <input type="hidden" name="id" value="<?= $user->id; ?>">
+            <form action="/sms/scripts/update-user.php" method="post">
+                <input type="text" class="user-info" required name="username" placeholder="Username" value="<?= $user->username ?>">
+                <input type="text" class="user-info" required name="role" placeholder="Role" value="<?= $user->role ?>">
+                <input type="hidden" name="id" value="<?= $user->id; ?>">
 
-            <input type="submit" class="btn btn-edit" value="Update User">
-            <input type="submit" class="btn btn-delete" formaction="/sms/scripts/delete-user.php" value="Delete User">
-        </form>
+                <input type="submit" class="btn btn-edit" value="Update User">
+                <input type="submit" class="btn btn-delete" formaction="/sms/scripts/delete-user.php" value="Delete User">
+            </form>
 
-    <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
 </div>
 
-
-<div id="admin-product-container">
-
-    <h2>FOR RODUCTS</h2>
-    <div class="product-form-container">
-        <form action="/sms/scripts/post-product.php" method="post" enctype="multipart/form-data">
-            <input type="text" name="title" placeholder="Name">
-            <input type="number" name="price" placeholder="Price">
-            <textarea type="text" name="description" rows="5" cols="100" placeholder="Description"></textarea>
-            <input type="file" name="image" accept="image/*" ><br>
-            <input type="submit" value="Save">
-        </form>
-    </div>
-
-    <table class="products-table">
-        <thead>
-            <tr>
-                <th class="products-table-head">Name</th>
-                <th class="products-table-head">Description</th>
-                <th class="products-table-head">Price</th>
-                <th class="products-table-head">Img</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($products as $product) : ?>
-                <tr>
-                <form action="/sms/scripts/post-edit-product.php" method="post" enctype="multipart/form-data">
-                <td>
-                    <input type="text" name="title" placeholder="Tilte" value="<?= $product->title?>"><br>
-                </td>
-                <td>
-                    <textarea name="description" placeholder="Description"><?= $product->description?></textarea>
-                </td>
-                <td>
-                    <input type="number" name="price" placeholder="Price" value="<?= $product->price?>">
-                </td>
-                <td>
-                <input type="file" name="image" accept="image/*">
-                </td>
-                <input type="hidden" name="id" value="<?= $product->id; ?>">
-                <td>
+<button class="accordion">Manage products:
+    <p class="underrubrik"> Click to add a new product or edit the products </p>
+</button>
+<div class="panel">
+    <div id="admin-product-container">
+        <!-- <h2>FOR RODUCTS</h2> -->
+        <!-- <div class="product-form-container">
+            <form action="/sms/scripts/post-product.php" method="post" enctype="multipart/form-data">
+                <input type="text" name="title" placeholder="Name">
+                <input type="number" name="price" placeholder="Price">
+                <textarea type="text" name="description" rows="5" cols="100" placeholder="Description"></textarea>
+                <input type="file" name="image" accept="image/*"><br>
                 <input type="submit" value="Save">
-                </td>
+            </form>
+        </div> -->
 
-                </form>
-
-                <td>
-                <form action="/sms/scripts/delete-product.php" method="post">
-                            <input type="hidden" name="id" value="<?= $product->id ?>">
-                            <input class="btn-add" type="submit" value="Delete">
-                </form>
-                </td>
-
-
+        <table class="products-table">
+            <thead>
+                <tr>
+                    <th class="products-table-head">Name</th>
+                    <th class="products-table-head">Description</th>
+                    <th class="products-table-head">Price</th>
+                    <th class="products-table-head">Img</th>
                 </tr>
-                
+            </thead>
+            <tbody>
+                <?php foreach ($products as $product) : ?>
+                    <tr>
+                        <form action="/sms/scripts/post-edit-product.php" method="post" enctype="multipart/form-data">
+                            <td>
+                                <input type="text" name="title" placeholder="Tilte" value="<?= $product->title ?>"><br>
+                            </td>
+                            <td>
+                                <textarea name="description" placeholder="Description"><?= $product->description ?></textarea>
+                            </td>
+                            <td>
+                                <input type="number" name="price" placeholder="Price" value="<?= $product->price ?>">
+                            </td>
+                            <td>
+                                <input type="file" name="image" accept="image/*">
+                            </td>
+                            <input type="hidden" name="id" value="<?= $product->id; ?>">
+                            <td>
+                                <input type="submit" value="Save">
+                            </td>
 
-<!-- 
+                        </form>
+
+                        <td>
+                            <form action="/sms/scripts/delete-product.php" method="post">
+                                <input type="hidden" name="id" value="<?= $product->id ?>">
+                                <input class="btn-add" type="submit" value="Delete">
+                            </form>
+                        </td>
+
+
+                    </tr>
+
+
+                    <!-- 
                 <tr>
                     <td>
                         <p>IMAGE</p>
@@ -151,77 +159,94 @@ if (!$isLoggedIn || !$isAdmin) {
 
                     </td>
                 </tr> -->
-            <?php endforeach; ?>
-        </tbody>
+                <?php endforeach; ?>
+            </tbody>
 
-    </table>
+        </table>
+
+        <div class="product-form-container">
+            <form action="/sms/scripts/post-product.php" method="post" enctype="multipart/form-data">
+                <input type="text" name="title" placeholder="Name">
+                <input type="number" name="price" placeholder="Price">
+                <textarea type="text" name="description" rows="5" cols="100" placeholder="Description"></textarea>
+                <input type="file" name="image" accept="image/*"><br>
+                <input type="submit" value="Save">
+            </form>
+        </div>
+
+
+    </div>
+
 </div>
 
-<hr>
-
-<div id="admin-order-container">
-    <div class="admin-order-wrapper">
-        <fieldset class="order-fieldset">
-            <legend class="order-legend">Orders</legend>
-            <table class="order-table">
-                <thead>
-                    <tr>
-                        <th class="order-table-head">Order #</th>
-                        <th class="order-table-head">Customer</th>
-                        <th class="order-table-head">Date</th>
-                        <th class="order-table-head">Status</th>
-                        <th class="order-table-head">Update</th>
-                        <th class="order-table-head">Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($orders as $order) : var_dump($order) ?>
+<button class="accordion">Manage orders:
+    <p class="underrubrik">Click to update orders</p>
+</button>
+<div class="panel">
+    <!-- <hr> -->
+    <div id="admin-order-container">
+        <div class="admin-order-wrapper">
+            <fieldset class="order-fieldset">
+                <legend class="order-legend">Orders</legend>
+                <table class="order-table">
+                    <thead>
                         <tr>
-                            <td>
-                                <p>
-                                    <?= $order->id ?>
-                                </p>
-                            </td>
-                            <td>
-                                <p>
-                                    <?= $order->user_id ?>
-                                </p>
-                            </td>
-                            <td>
-                                <p>
-                                    <?= $order->date ?>
-                                </p>
-                            </td>
-                            <td>
-                                <form action="/sms/scripts/post-edit-order.php" method="post"> 
-                                    <select name="order-status">
-                                        <option value=""><?= $order->status ?></option>
-                                        <?php foreach ($statuses as $status) : ?>
-                                            <option name="order-status" value="<?= $status->status; ?>"><?= $status; ?></option>
-                                        <?php endforeach; ?>
-                                        <input type="hidden" name="order-id" value="<?= $order->id ?>"> 
-                                    </select>
-                                    
-                                    <input type="submit" value="Update">
-                                </form>
-                            </td>
-                            <td>
-                                <p></p>
-                            </td>
-                            <td>
-                                <form action="/sms/scripts/post-delete-order.php" method="post">
-                                    <input type="hidden" name="order-id" value="<?= $order->id ?>">
-                                    <input class="order-btn btn-delete" type="submit" value="Delete">
-                                </form>
-                            </td>
+                            <th class="order-table-head">Order #</th>
+                            <th class="order-table-head">Customer</th>
+                            <th class="order-table-head">Date</th>
+                            <th class="order-table-head">Status</th>
+                            <!-- <th class="order-table-head">Update</th> -->
+                            <th class="order-table-head">Delete</th>
                         </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($orders as $order) : /*var_dump($order)*/ ?>
+                            <tr>
+                                <td>
+                                    <p>
+                                        <?= $order->id ?>
+                                    </p>
+                                </td>
+                                <td>
+                                    <p>
+                                        <?= $order->user_id ?>
+                                    </p>
+                                </td>
+                                <td>
+                                    <p>
+                                        <?= $order->date ?>
+                                    </p>
+                                </td>
+                                <td>
+                                    <form action="/sms/scripts/post-edit-order.php" method="post">
+                                        <select name="order-status">
+                                            <option value=""><?= $order->status ?></option>
+                                            <?php foreach ($statuses as $status) : ?>
+                                                <option name="order-status" value="<?= $status->status; ?>"><?= $status; ?></option>
+                                            <?php endforeach; ?>
+                                            <input type="hidden" name="order-id" value="<?= $order->id ?>">
+                                        </select>
+
+                                        <input type="submit" value="Update">
+                                    </form>
+                                </td>
+                                <!-- <td>
+                                    <p></p>
+                                </td> -->
+                                <td>
+                                    <form action="/sms/scripts/post-delete-order.php" method="post">
+                                        <input type="hidden" name="order-id" value="<?= $order->id ?>">
+                                        <input class="order-btn btn-delete" type="submit" value="Delete">
+                                    </form>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
 
 
-            <-- BACKUP TABLE DATA -->
-                <!-- <table class="order-table">
+                <-- BACKUP TABLE DATA -->
+                    <!-- <table class="order-table">
                 <thead>
                     <tr>
                         <th class="order-table-head">Order #</th>
@@ -262,9 +287,12 @@ if (!$isLoggedIn || !$isAdmin) {
                 <?php endforeach; ?>
                 </tbody>
             </table> -->
-        </fieldset>
+            </fieldset>
+        </div>
     </div>
+
 </div>
+
 
 <?php
 Template::footer();
